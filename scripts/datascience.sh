f72add406566f74ea494fbc792aa111f75131972
Clément Trassoudaine <clement.trassoudaine@imt-atlantique.net>
2018-01-16 22:36:50 +0100

    fix

--- scripts/datascience.sh
+++ scripts/datascience.sh
@@ -4,8 +4,9 @@ apt install -y \
     python3-matplotlib \
     python3-pandas \
     python3-seaborn \
-    python-sklearn \
-    ipyparallel
+    python-sklearn
+
+pip3 install ipyparallel
 
 ipcluster nbextension enable
     
